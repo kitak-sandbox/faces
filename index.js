2931ea5037a14e0a69c26e635d5acc077e3b745c
Keisuke KITA <kei.kita2501@gmail.com>
2014-11-22 19:34:55 +0900

    videoタグからwebカメラのキャプチャを撮って、socket.ioのクライアントコードが取得できることを確認するまで

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.createServer(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
-app.listen(8080);
+server.listen(8080);
