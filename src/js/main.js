fad494fff8bc825c654d4f915ce72891b29a38ff
Keisuke KITA <kei.kita2501@gmail.com>
2014-11-23 15:12:31 +0900

    IPアドレスがベタ書きになっていたのでlocalhostにした

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ window.onload = function () {
   var localMediaStream = null;
   var streaming = false;
   var uid = cookie.parse(document.cookie).uid;
-  var socket = io('http://192.168.13.51:8080/');
+  var socket = io('http://localhost:8080/');
 
   canvas.dataset.uid = uid;
 
